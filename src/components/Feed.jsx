7fd7bd95709a520d300671c2951156647de5cb1d
GurnoorArora <gurnoora51@gmail.com>
2025-07-24 02:27:08 +0530

    feat:added feed functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants.js';
+import { addFeed } from '../utils/feedSlice.js';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed=useSelector((state) => state.feed);
+    console.log('Feed data:', feed);
+
+   const dispatch = useDispatch();
+  const getFeedData = async () => {
+    if(feed) return;
+    try {
+      const res = await axios.get(`${BASE_URL}/feed`, { withCredentials: true });
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.error('Error fetching feed:', error);
+    }
+  };
+  useEffect(() => {
+    getFeedData();
+  },[]);
+  return <div>Feed</div>;
+};
+
 
 export default Feed
